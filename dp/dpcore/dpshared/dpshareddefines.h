e8bf539d2d8f1614fd1b8c8f2bff0a94e77c4919
richard <richard@debian>
2016-02-21 15:35:46 -0600

    3

--- dp/dpcore/dpshared/dpshareddefines.h
+++ dp/dpcore/dpshared/dpshareddefines.h
@@ -27,6 +27,24 @@
     #define dpshared_writeLock_block( x ) x->_usemacro_writeLock()
 #endif
 
+
+#ifdef dpshared_debug
+    #define dpshared_guard_tryReadLock( x, y ) x->_guard_tryReadLock( y, __FILE__, __LINE__, __func__ )
+    #define dpshared_guard_tryWriteLock( x, y ) x->_guard_tryWriteLock( y, __FILE__, __LINE__, __func__ )
+    #define dpshared_guard_tryReadLock_timeout( x, y, timeout_ms ) x->_guard_tryReadLock( y, timeout_ms, __FILE__, __LINE__, __func__ )
+    #define dpshared_guard_tryWriteLock_timeout( x, y, timeout_ms ) x->_guard_tryWriteLock( y, timeout_ms, __FILE__, __LINE__, __func__ )
+    #define dpshared_guard_readLock_block( x, y ) x->_guard_readLock( y, __FILE__, __LINE__, __func__ )
+    #define dpshared_guard_writeLock_block( x, y ) x->_guard_writeLock( y, __FILE__, __LINE__, __func__ )
+#else
+    #define dpshared_guard_tryReadLock( x, y ) x->_usemacro_tryReadLock( y )
+    #define dpshared_guard_tryWriteLock( x, y ) x->_usemacro_tryWriteLock( y )
+    #define dpshared_guard_tryReadLock_timeout( x, y, timeout_ms ) x->_usemacro_tryReadLock( y, timeout_ms )
+    #define dpshared_guard_tryWriteLock_timeout( x, y, timeout_ms ) x->_usemacro_tryWriteLock( y, timeout_ms )
+    #define dpshared_guard_readLock_block( x, y ) x->_usemacro_readLock( y )
+    #define dpshared_guard_writeLock_block( x, y ) x->_usemacro_writeLock( y )
+#endif
+
+
 #endif
 
 
